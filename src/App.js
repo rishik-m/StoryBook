90ea9e174b1d9ea615f2dfbe612cf7ec8106c905
rishik-m <rishik.makhija2000@gmail.com>
2021-08-13 07:21:54 +0530

    header done

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import Header from './Header';
 function App() {
   return (
     <div className="app">
-       <h1> StoryBook </h1>
        <Header />
     </div>
   );
--- src/Header.js
+++ src/Header.js
@@ -1,6 +1,16 @@
 import React from 'react'
 import './Header.css'
 import SearchIcon from '@material-ui/icons/Search';
+import HomeIcon from '@material-ui/icons/Home';
+import SupervisedUserCircleIcon from '@material-ui/icons/SupervisedUserCircle';
+import StorefrontIcon from '@material-ui/icons/Storefront';
+import SubscriptionsIcon from '@material-ui/icons/Subscriptions';
+import MenuIcon from '@material-ui/icons/Menu';
+import { Avatar, IconButton } from '@material-ui/core';
+import ListIcon from '@material-ui/icons/List';
+import MessageIcon from '@material-ui/icons/Message';
+import NotificationsNoneIcon from '@material-ui/icons/NotificationsNone';
+import ArrowDropDownIcon from '@material-ui/icons/ArrowDropDown';
 
 function Header() {
     return (
@@ -12,11 +22,36 @@ function Header() {
                 />
                 <div className= "header-input">
                     <SearchIcon />
-                    <input type= "text" />
+                    <input type= "text" placeholder= "Search StoryBook" />
                 </div>
             </div>
-            <div className= "header-cent"></div>
-            <div className= "header-right"></div>
+            <div className= "header-cent">
+                <div className= "header-icons header-icons--active">
+                    <HomeIcon fontSize= "large" />
+                </div>
+                <div className= "header-icons">
+                    <SupervisedUserCircleIcon fontSize= "large" />
+                </div>
+                <div className= "header-icons">
+                    <StorefrontIcon fontSize= "large" />
+                </div>
+                <div className= "header-icons">
+                    <SubscriptionsIcon fontSize= "large" />
+                </div>
+                <div className= "header-icons">
+                    <MenuIcon fontSize= "large" />
+                </div>
+            </div>
+            <div className= "header-right">
+                <div className= "header_right-options">
+                   <Avatar />
+                   <h4> Rishik </h4>
+                </div>
+                <IconButton> <ListIcon /> </IconButton>
+                <IconButton> <MessageIcon /> </IconButton>
+                <IconButton> <NotificationsNoneIcon /> </IconButton>
+                <IconButton> <ArrowDropDownIcon /> </IconButton>
+            </div>
         </div>
     )
 }
