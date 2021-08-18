0002ace28ac247bcc6931eb77a2326848285572f
rishik-m <rishik.makhija2000@gmail.com>
2021-08-18 07:55:16 +0530

    leftbar added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import './App.css';
 import Header from './Header';
+import Leftbar from './Leftbar';
 
 function App() {
   return (
     <div className="app">
        <Header />
+       <div className= "app-structure">
+         <Leftbar />
+       </div>
     </div>
   );
 }
