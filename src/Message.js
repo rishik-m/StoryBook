1221b2460df7ea6680512f4a2f5088ba04a1cae3
rishik-m <rishik.makhija2000@gmail.com>
2021-08-21 04:58:18 +0530

    Post section updates

--- src/Message.js
+++ src/Message.js
@@ -1,6 +1,9 @@
 import { Avatar } from '@material-ui/core';
 import React from 'react'
 import './Message.css';
+import ThumbUpIcon from '@material-ui/icons/ThumbUp';
+import ChatBubbleOutlineIcon from '@material-ui/icons/ChatBubbleOutline';
+import NearMeIcon from '@material-ui/icons/NearMe';
 
 function Message({ username, profile, image, text, time }) {
     return (
@@ -17,6 +20,20 @@ function Message({ username, profile, image, text, time }) {
             </div>
             <div className= "message-img">
                <img src= {image} alt= "" />
+            </div>
+            <div className= "message-options">
+                <div className= "message-option">
+                    <ThumbUpIcon />
+                    <p> Like </p>
+                </div>
+                <div className= "message-option">
+                    <ChatBubbleOutlineIcon />
+                    <p> Comment </p>
+                </div>
+                <div className= "message-option">
+                    <NearMeIcon />
+                    <p> Share </p>
+                </div>
             </div>
         </div>
     )
