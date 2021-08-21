7a5176e5ae3e921ba57e3df782871b455a252fee
rishik-m <rishik.makhija2000@gmail.com>
2021-08-21 07:35:39 +0530

    firebase auth configured

--- src/Firebase.js
+++ src/Firebase.js
@@ -1,9 +1,19 @@
+import firebase from 'firebase';
+
 const firebaseConfig = {
-    apiKey: "AIzaSyBapseTRcbR4T_2hGqV-92piusQZqtnJFs",
-    authDomain: "storybook-9f5c5.firebaseapp.com",
-    projectId: "storybook-9f5c5",
-    storageBucket: "storybook-9f5c5.appspot.com",
-    messagingSenderId: "726390945074",
-    appId: "1:726390945074:web:38e226594d5b19d2b6932b",
-    measurementId: "G-WD01GWDGBD"
+   apiKey: "AIzaSyBapseTRcbR4T_2hGqV-92piusQZqtnJFs",
+   authDomain: "storybook-9f5c5.firebaseapp.com",
+   projectId: "storybook-9f5c5",
+   storageBucket: "storybook-9f5c5.appspot.com",
+   messagingSenderId: "726390945074",
+   appId: "1:726390945074:web:38e226594d5b19d2b6932b",
+   measurementId: "G-WD01GWDGBD"
 };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+const provider = new firebase.auth.GoogleAuthProvider();
+
+export { auth, provider };
+export default db;
--- src/Login.js
+++ src/Login.js
@@ -1,11 +1,16 @@
 import { Button } from '@material-ui/core';
 import React from 'react'
 import './Login.css';
+import { auth, provider } from './firebase';
 
 function Login() {
 
     const signIn = () => {
-        //SIGNIN
+        auth.signInWithPopup(provider)
+        .then(result => {
+            console.log(result);
+        })
+        .catch(error => alert(error.message));
     }
 
     return (
